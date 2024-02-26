2ccb97fb3c3469f01d46346b3e7d351ccd2e4420
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2024-02-26 18:13:50 -0500

    Router
    Se crea componente contact page y se define en las rutas

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,23 +4,26 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent,
     AboutPageComponent,
-    SidebarComponent
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports:[
-    HomePageComponent,
     AboutPageComponent,
-    SidebarComponent
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
   ]
 })
 export class SharedModule { }
