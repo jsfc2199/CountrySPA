6319993392be67f2bcda8c3e8d682b34506c2dc4
Juanse Franco <jsfc2199@gmail.com>
2024-06-22 17:43:21 -0500

    Router/Lazy Loading
    Se aplica lazy loading para cargar de manera lazy el modulo de cpuntries

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
     component: ContactPageComponent,
   },
   {
+    path: 'countries',
+    loadChildren: () => import('./countries/countries.module').then(m => m.CountriesModule) //aplicamos lazy loading
+  },
+  {
     path: '**',
     redirectTo: '',
   },
