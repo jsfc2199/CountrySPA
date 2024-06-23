af281107ff8e866ad645cbd6de8b508b3d601c7d
Juanse Franco <jsfc2199@gmail.com>
2024-06-22 20:22:26 -0500

    Debounce implementado

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -37,7 +37,7 @@ export class CountriesService {
     return this.http.get<Country[]>(url)
     .pipe(
       catchError(error => of([])),
-      delay(2000),
+      // delay(2000), //ya no lo ocupamos por el debounce
     )
   }
 }
--- src/app/shared/components/SearchBox/SearchBox.component.ts
+++ src/app/shared/components/SearchBox/SearchBox.component.ts
@@ -1,21 +1,41 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
 })
 export class SearchBoxComponent {
+  constructor() {}
 
-  constructor() { }
+  //observable "manual"
+  private debouncer: Subject<string> = new Subject();
+
+  ngOnInit(): void {
+    this.debouncer
+    .pipe(
+      debounceTime(500) //si por 2 segundos no se emiten valores, lo emitirá con lo que haya
+    )
+    .subscribe(value => {
+      this.onDebounce.emit(value)
+    })
+  }
 
   @Input()
-  placeholder: string = ''
+  placeholder: string = '';
 
   @Output()
   onValue: EventEmitter<string> = new EventEmitter();
 
-  emitValue(term: string){
-    this.onValue.emit(term)
+  @Output()
+  onDebounce: EventEmitter<string> = new EventEmitter();
+
+  emitValue(term: string) {
+    this.onValue.emit(term);
   }
 
+  onKeyPress(term: string) {
+    //debounce para que si el usuario escribe pero no da enter, al cabo de un tiempo haga la búsqueda
+    this.debouncer.next(term)
+  }
 }
